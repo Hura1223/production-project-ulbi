4e0b3ea4f037b13b593b824020559dafaac6f9e9
HuraMikola <kolyagura569@ukr.net>
2025-12-25 11:55:45 +0200

    roating was added,  tried Suspense  for artificial delay

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,25 @@
 // react
-import { FC } from "react";
+import { FC, Suspense } from "react";
 // styles
 import "../../index.scss";
-// ui
-import { Counter } from "../counter/Counter";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "../../pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "../../pages/MainPage/MainPage.async";
 
 interface AppProps {}
 
 export const App: FC<AppProps> = ({}) => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={"/"}>Головна</Link>
+      <Link to={"/about"}>Про сайт</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+// react
+import { FC } from "react";
+// styles
+// import styles from "./AboutPage.module.scss";
+
+interface AboutPageProps {}
+
+const AboutPage: FC<AboutPageProps> = ({}) => {
+  return <div>About page</div>;
+};
+
+export default AboutPage;
